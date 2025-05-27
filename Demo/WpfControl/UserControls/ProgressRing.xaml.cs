97a4b4d925daf1c872ee920e5d425697e21bc591
JYJ <2534803898@qq.com>
2025-05-27 15:35:02 +0800

    提交圆环进度条

---
+++ Demo/WpfControl/UserControls/ProgressRing.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControl.UserControls
+{
+    /// <summary>
+    /// ProgressRing.xaml 的交互逻辑
+    /// </summary>
+    public partial class ProgressRing : UserControl
+    {
+        public static readonly DependencyProperty ProgressProperty =
+            DependencyProperty.Register("Progress", typeof(double), typeof(ProgressRing),
+                new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public double Progress
+        {
+            get => (double)GetValue(ProgressProperty);
+            set => SetValue(ProgressProperty, value);
+        }
+
+        public ProgressRing()
+        {
+            Resources.Add("ProgressToDashArrayConverter", new ProgressToDashArrayConverter());
+
+            InitializeComponent();
+        }
+    }
+
+    // 值转换器
+    public class ProgressToDashArrayConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values.Length < 2 ||
+                !(values[0] is double progress) ||
+                !(values[1] is double actualWidth))
+                return DependencyProperty.UnsetValue;
+
+            progress = Math.Max(0, Math.Min(100, progress));
+            var circumference = Math.PI * (actualWidth - 10); // 周长 = π*(直径)
+            var dashLength = (progress / 1000) * circumference;
+
+            return new DoubleCollection { dashLength, circumference };
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Demo/WpfControl/ViewModels/ProgressViewModel .cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WpfControl.ViewModels
+{
+    public class ProgressViewModel : INotifyPropertyChanged
+    {
+        private double _progress;
+        public double Progress
+        {
+            get => _progress;
+            set
+            {
+                _progress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // 模拟进度更新
+        public void StartSimulation()
+        {
+            var timer = new Timer(500);
+            timer.Elapsed += (s, e) => Progress = (Progress + 1) % 100;
+            timer.Start();
+        }
+    }
+}
